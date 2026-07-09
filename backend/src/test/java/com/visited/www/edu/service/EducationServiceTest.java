429246bc687819acb62dbb4e8de1b18bccb10850
Kim-buyeon <bu2000kim@gmail.com>
2026-07-09 17:29:45 +0900

    fix: EducationSeriviceTest에 LocalDateTime import 추가(EDU-FR-009)

--- backend/src/test/java/com/visited/www/edu/service/EducationServiceTest.java
+++ backend/src/test/java/com/visited/www/edu/service/EducationServiceTest.java
@@ -40,6 +40,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
